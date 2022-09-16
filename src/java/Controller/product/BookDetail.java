5721b8799d2dd18df8f905c07ceda8d2b76964dd
Đức Trịnh <you@example.com>
2022-09-16 18:01:40 +0700

    viewbook1

---
+++ src/java/Controller/product/BookDetail.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package Controller.product;
+
+import Model.Book;
+import context.BookDAO;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ttaad
+ */
+@WebServlet(name="BookDetail", urlPatterns={"/BookDetail"})
+public class BookDetail extends HttpServlet {
+   
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        int id=Integer.parseInt(request.getParameter("id"));
+        BookDAO b=new BookDAO();
+        Book thisbook=b.getBookById(id);
+        int cateid=thisbook.getCategoryid();
+            
+        
+    } 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
